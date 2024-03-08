f19d6c4dc82b97a9007d6357ddf7c7f7bb2441e9
Mudassir Fazal <162458958+mudassirfazal432@users.noreply.github.com>
2024-03-08 20:47:26 +0500

    Add files via upload

---
+++ m.js
@@ -0,0 +1,66 @@
+//addition//
+console.log(5 + 3);
+//subtraction//
+console.log(7 - 2);
+//multiplication//
+console.log(2 * 5);
+//division//
+console.log(10 / 2);
+//two variables addition//
+var number1 = 4;
+var number2 = 6;
+var add = number1 + number2;
+console.log(add);
+//two variables subtraction//
+var number3 = 9;
+var number4 = 2;
+var sub = number3 - number4;
+console.log(sub);
+//two variables multiplication//
+var number5 = 8;
+var number6 = 2;
+var mul = number5 * number6;
+console.log(mul);
+//two variables division//
+var number7 = 12;
+var number8 = 2;
+var div = number7 / number8;
+console.log(div);
+//value add, sub, mul, div//
+var a = 5;
+a = 5 + 3;
+var b = 4;
+b = 4 - 2;
+var c = 10;
+c = 10 * 3;
+var d = 16;
+d = 16 / 2;
+console.log(a, b, c, d);
+//modulous % //
+var number9 = 21;
+var number10 = 5;
+var per = (number9 % number10);
+console.log(per);
+//name concatenation//
+var m = "mudassir";
+var f = "fazal";
+console.log(m + f);
+//first and last name with space//
+var r = "mudassir";
+var l = "fazal";
+console.log(r + ' ' + l);
+//true/false//
+var digit1 = 12;
+var digit2 = 12;
+console.log(digit1 == digit2);
+var digit3 = 12;
+var digit4 = 13;
+console.log(digit3 == digit4);
+//value of power//
+var number11 = 21;
+var number12 = 5;
+var power = (Math.pow(number11, number12));
+console.log(power);
+//comparison operator//
+console.log(7 < 6);
+console.log(7 > 6);
---
+++ m.ts
@@ -0,0 +1,81 @@
+//addition//
+console.log(5 + 3)
+
+//subtraction//
+console.log(7 - 2)
+
+//multiplication//
+console.log(2 * 5)
+
+//division//
+console.log(10 / 2)
+
+//two variables addition//
+let number1 = 4
+let number2 = 6
+let add = number1 + number2
+console.log(add)
+
+//two variables subtraction//
+let number3 = 9
+let number4 = 2
+let sub = number3 - number4
+console.log(sub)
+
+//two variables multiplication//
+let number5 = 8
+let number6 = 2
+let mul = number5 * number6
+console.log(mul)
+
+//two variables division//
+let number7 = 12
+let number8 = 2
+let div = number7 / number8
+console.log(div)
+
+//value add, sub, mul, div//
+let a = 5
+a = 5 + 3
+let b = 4
+b = 4 - 2
+let c = 10
+c = 10 * 3
+let d = 16
+d = 16 / 2
+console.log(a, b, c, d)
+
+//modulous % //
+let number9 = 21
+let number10 = 5
+let per = (number9 % number10)
+console.log(per)
+
+//name concatenation//
+let m = "mudassir"
+let f = "fazal"
+console.log(m + f)
+
+//first and last name with space//
+let r = "mudassir"
+let l = "fazal"
+console.log(r + ' ' + l)
+
+
+//true/false//
+let digit1 = 12
+let digit2 = 12
+console.log(digit1 == digit2)
+let digit3 = 12
+let digit4 = 13
+console.log(digit3 == digit4)
+
+//value of power//
+let number11 = 21
+let number12 = 5
+let power = (number11 ** number12)
+console.log(power)
+
+//comparison operator//
+console.log (7<6)
+console.log (7>6)
